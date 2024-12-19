e6d33d65f2487f22e053f4e2d84aa24bb5388b17
Phoenixuu <dung2000xd@gmail.com>
2024-12-19 14:32:33 +0700

    update

--- mnist_ml/KNN/src/knn.cc
+++ mnist_ml/KNN/src/knn.cc
@@ -117,5 +117,72 @@ double knn::calculate_distance(data* query_point, data* input)
 return distance;
 }
 
-double knn::validate_performance();
-double knn::test_performance();
+double knn::validate_performance()
+{
+	double current_performance = 0;
+	int count = 0;
+	int data_index = 0;
+	for(data *query_point : *validation_data)
+	{
+		find_knearest(query_point);
+		int prediction = predict();
+		if(prediction == query_point-> get_label())
+		{
+			count++;
+		}
+		data_index++;
+		printf("Current Performance = %.3f %%\n", ((double)count*100.0)/((double)data_index));
+	}
+	current_performance = ((double)count*100.0)/((double)validation_data->size());
+	printf("Validation Performance for K = %d: %.3f %%\n", k, current_performance);
+	return current_performance;
+}
+double knn::test_performance()
+{
+	double current_performance = 0;
+	int count = 0;
+	for(data *query_point : *test_data)
+	{
+		find_knearest(query_point);
+		int prediction = predict();
+		if(prediction == query_point->get_label())
+		{
+			count++;
+		}
+	}
+	current_performance = ((double)count*100.0/((double)test_data->size()));
+	printf("Tested Performance = %.3f %%\n", current_performance);
+}
+int main()
+{
+	data_handler *dh = new data_handler();
+	dh->read_feature_vector("./train-images.idx3-ubyte");
+	dh->read_feature_labels("./train-labels.idx1-ubyte");
+	dh->split_data();
+	dh->count_classes();
+	knn *knearest = new knn();
+	knearest->set_training_data(dh->get_training_data());
+	knearest->set_validation_data(dh->get_validation_data());
+	knearest->set_test_data(dh->get_test_data());
+	double performance = 0;
+	double best_performance = 0;
+	int best_k = 1;
+	for(int i = 1; i <= 4; i++)
+	{
+		if(i == 1)
+		{
+			knearest->set_k(i);
+			performance = knearest->validate_performance();
+			best_performance = performance;
+		} else 
+		{
+			knearest->set_k(i);
+			performance = knearest->validate_performance();
+			if(performance > best_performance)
+			{
+				best_performance = performance;
+				best_k = i;
+			}
+		}
+	}
+}
